d4ff1f522ebf6c1bc925dd4821c070a39f4dc353
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2024-07-18 13:38:49 +0200

    Removed file

--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-import multiprocessing
-
-workers = multiprocessing.cpu_count() * 2 + 1
-bind = "0.0.0.0:8080"
-worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
-timeout = 300
